b4ce86d54c3a6c27ef0329d99018f6a296d24334
Pedro-Costa <71532131+Pedro-Maester@users.noreply.github.com>
2025-04-03 10:27:41 -0300

    Add files via upload

--- Show.java
+++ Show.java
@@ -2,8 +2,8 @@
 import java.util.Date;
 
 public class Show {
-    private String sid , tipo, titulo, rating , duration;
-    private String[] director , cast, country, listed_in;
+    private String sid , tipo, titulo, rating , duration,director;
+    private String[]  cast, country, listed_in;
     private int Release_year;
     private Date Date_added;
     public Show(String raw){
@@ -13,13 +13,18 @@ public class Show {
         i = j+1;
         j = raw.indexOf(',', i);
         this.tipo = raw.substring(i,j);
+        // consertar o titulo fdp 1103
         i = j+1;
         j = raw.indexOf(',', i);
         this.titulo = raw.substring(i,j);
         i = j+1;
         if (raw.charAt(i) == '\"'){
-            
+            j = raw.indexOf('\"', i);
+        }else{
+            j = raw.indexOf(',', i);
         }
+        this.director =  raw.substring(i,j);
+        MyIO.print(this.director+'\n');
 
         
     }
